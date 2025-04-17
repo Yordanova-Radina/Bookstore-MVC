fcaad3b2f26d8badcd439d5930f3291663c5b160
Yordanova-Radina <radina_yordanova@abv.bg>
2025-04-17 14:50:06 +0300

    Add routes in web.php

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('aboutus');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+Route::post(
+    'user/store',
+    [UserController::class, 'store']
+)->name('user.store');
+
+Route::get('/posts/add', function () {
+    return view('add-post');
+})->name('posts.add');
+
+Route::post(
+    '/post/store',
+    [PostController::class, 'store']
+)->name('posts.store');
+
+Route::get(
+    '/post/show/{post}',
+    [PostController::class, 'show']
+)->name('post.show');
+
+Route::get('/login', function () {})->name('login');
