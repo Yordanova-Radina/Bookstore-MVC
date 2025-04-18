e46937a7ac6c118c952060e6f81983df509899a0
Yordanova-Radina <radina_yordanova@abv.bg>
2025-04-18 15:36:46 +0300

    Edit css files

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,24 +1,24 @@
-<nav class="navbar navbar-fixed-top navbar-inverse">
+<nav class="navbar navbar-custom">
     <div class="container">
         <div class="navbar-header">
             <a class="navbar-brand" href="#">Светлина</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class="{{ Route::currentRouteName()== 'home' ? 'active' : '' }}">
-                    <a href="{{route('home')}}">Начало</a>
+                <li class="{{ Route::currentRouteName() == 'home' ? 'active' : '' }}">
+                    <a href="{{ route('home') }}">Начало</a>
                 </li>
-                <li class="{{ Route::currentRouteName()== 'aboutus' ? 'active' : '' }}">
-                    <a href="{{route('aboutus')}}">За нас</a>
+                <li class="{{ Route::currentRouteName() == 'aboutus' ? 'active' : '' }}">
+                    <a href="{{ route('aboutus') }}">За нас</a>
                 </li>
-                <li class="{{ Route::currentRouteName()== 'posts.add' ? 'active' : '' }}">
+                <li class="{{ Route::currentRouteName() == 'posts.add' ? 'active' : '' }}">
                     <a href="{{ route('posts.add') }}">Добави отзив</a>
                 </li>
-                <li class="{{ Route::currentRouteName()== 'register' ? 'active' : '' }}">
-                    <a href="{{route('register')}}">Регистрация</a>
+                <li class="{{ Route::currentRouteName() == 'register' ? 'active' : '' }}">
+                    <a href="{{ route('register') }}">Регистрация</a>
                 </li>
-                <li class="{{ Route::currentRouteName()== 'login' ? 'active' : '' }}">
-                    <a href="{{route('login')}}">Вход</a>
+                <li class="{{ Route::currentRouteName() == 'login' ? 'active' : '' }}">
+                    <a href="{{ route('login') }}">Вход</a>
                 </li>
             </ul>
         </div>
